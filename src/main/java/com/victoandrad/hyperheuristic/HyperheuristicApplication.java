e52c7ee0ff875ca10330800bed5aa91dd7b49e00
Victor Andrade <victor64gaming@gmail.com>
2025-02-03 20:05:20 -0300

    standard maven project structure

---
+++ src/main/java/com/victoandrad/hyperheuristic/HyperheuristicApplication.java
@@ -0,0 +1,13 @@
+package com.victoandrad.hyperheuristic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyperheuristicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyperheuristicApplication.class, args);
+	}
+
+}
