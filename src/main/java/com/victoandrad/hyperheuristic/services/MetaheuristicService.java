8150e4a129738bf2d57259686b2f2cf79cf62900
Victor Andrade <victor64gaming@gmail.com>
2025-02-03 20:10:27 -0300

    metaheuristic service to make requests

---
+++ src/main/java/com/victoandrad/hyperheuristic/services/MetaheuristicService.java
@@ -0,0 +1,81 @@
+package com.victoandrad.hyperheuristic.services;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.victoandrad.hyperheuristic.heuristics.metaheuristics.Metaheuristic;
+import com.victoandrad.hyperheuristic.utils.Job;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class MetaheuristicService {
+
+    private final RestTemplate restTemplate;
+    private final ObjectMapper objectMapper;
+
+    @Autowired
+    public MetaheuristicService(RestTemplate restTemplate,
+                                ObjectMapper objectMapper) {
+        this.restTemplate = restTemplate;
+        this.objectMapper = objectMapper;
+    }
+
+    public Job applyMetaHeuristic(Metaheuristic metaHeuristic, JsonNode problem) {
+        System.out.println("Starting " + metaHeuristic.getClass().getSimpleName() + " meta-heuristic");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<JsonNode> request = new HttpEntity<>(problem, headers);
+
+        String jobId = restTemplate.exchange(
+                "http://localhost:8080/timetables/" + metaHeuristic.getName(),
+                HttpMethod.POST,
+                request,
+                String.class).getBody();
+
+        System.out.println("Job: " + jobId);
+
+        JsonNode job = null;
+
+        boolean isSolving = true;
+        while (isSolving) {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                System.out.println("Error while sleep: " + e.getMessage());
+            }
+
+            job = getJob(jobId);
+            String solverStatus = job.get("solverStatus").asText();
+            if (solverStatus.equals("NOT_SOLVING")) {
+                isSolving = false;
+            }
+        }
+
+        return new Job(jobId, job);
+    }
+
+    public JsonNode getJob(String jobId) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Accept", "application/json");
+        HttpEntity<Void> request = new HttpEntity<>(headers);
+
+        JsonNode problem = null;
+        try {
+            problem = objectMapper.readTree(restTemplate.exchange(
+                    "http://localhost:8080/timetables/" + jobId,
+                    HttpMethod.GET,
+                    request,
+                    String.class).getBody());
+        } catch (JsonProcessingException e) {
+            System.out.println("Error converting problem to JSON: " + e.getMessage());
+        }
+        return problem;
+    }
+}
