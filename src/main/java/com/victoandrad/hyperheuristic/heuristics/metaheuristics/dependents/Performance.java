c4dfb175cef8310831c75c5307110780dea7c8d2
Victor Andrade <victor64gaming@gmail.com>
2025-02-24 19:33:20 -0300

    Performance: arrumando erro de quando tinha pontuação init

--- src/main/java/com/victoandrad/hyperheuristic/heuristics/metaheuristics/dependents/Performance.java
+++ src/main/java/com/victoandrad/hyperheuristic/heuristics/metaheuristics/dependents/Performance.java
@@ -15,8 +15,14 @@ public class Performance {
 
     public static Performance of(String performance) {
         String[] parts = performance.split("/");
-        int hard = Integer.parseInt(parts[0].replace("hard", ""));
-        int soft = Integer.parseInt(parts[1].replace("soft", ""));
+        int hard, soft;
+        if (parts[0].contains("init")) {
+            hard = Integer.parseInt(parts[1].replace("hard", ""));
+            soft = Integer.parseInt(parts[2].replace("soft", ""));
+        } else {
+            hard = Integer.parseInt(parts[0].replace("hard", ""));
+            soft = Integer.parseInt(parts[1].replace("soft", ""));
+        }
         return new Performance(hard, soft);
     }
 }
